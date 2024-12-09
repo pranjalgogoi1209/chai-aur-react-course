66d41e6d17c5ddd05db5b9510d278c8dfd269212
pranjalgogoi1209 <gogoipranjal2022@gmail.com>
2024-12-09 23:51:34 +0530

    update part-01

---
+++ part-02/basic-react/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Chai aur React (npx create-react-app appname) | Pranjal Gogoi</h1>;
+}
+
+export default App;
--- part-2/01vitereact/src/App.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1>Chai aur React with vite + react app | Pranjal Gogoi</h1>;
-}
-
-export default App;
